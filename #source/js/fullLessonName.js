f627c9fcd9d56cce8267c9d5503cd2212e5c8437
botq1t <77788765+botq1t@users.noreply.github.com>
2021-08-31 13:14:46 +0300

    short teachers & header pulsing

--- #source/js/fullLessonName.js
+++ #source/js/fullLessonName.js
@@ -3,7 +3,38 @@ let lessonName = {
 	'full': ['Фразеология радиообмена на английском языке', 'Авиационная инженерная психология и человеческий фактор', 'Предотвращение АП при обслуживании ВД', 'Правила, процедуры и технология обслуживания воздушного движения', 'Экономика отрасли', 'Организация потоков воздушного движения', 'Авиационные приборы и ПНК', 'Метеорологическое обеспечение международной АН']
 };
 
+let teacherName = {
+	'short': ['Лазовский Г.Б., Швайко Е. П.', 'Науменко А.И.', 'Козлова О.Г.', 'Александров О.В.', 'Дубовский А.В.', 'Лазовский Г.Б.', 'Вишневский Р.А.', 'Пилипчук В.С.', 'Барабан И.И.'],
+	'full': ['Лазовский Георгий Борисович, Швайко Елена Петровна', 'Науменко Александр Иванович', 'Козлова Ольга Григорьевна', 'Пока хз', 'Дубовский Алексей Викторович', 'Лазовский Георгий Борисович', 'Вишневский Роман Анатольевич', 'Пилипчук Владимир Сергеевич', 'Барабан Иван Иванович']
+}
+
 $(document).ready(function () {
+	/*	console.log($('.ttest').text())
+		if (teacherName['short'][8] == $('.ttest').text()) {
+			console.log('true')
+		} else {
+			console.log('false')
+		}*/
+	function fullTeacherName() {
+		// console.log($(this).text());
+		console.log($(this).text());
+		for (let i = 0; i < teacherName['short'].length; i++) {
+			switch ($(this).text()) {
+				case teacherName['short'][i]:
+					$(this).fadeOut(100, function () {
+						$(this).text(teacherName['full'][i]).fadeIn(100)
+					})
+					break;
+
+				case teacherName['full'][i]:
+					$(this).fadeOut(100, function () {
+						$(this).text(teacherName['short'][i]).fadeIn(100)
+					})
+					break;
+			};
+		};
+	}
+
 	function fullLessonName() {
 		// console.log($(this).text())
 		for (let i = 0; i < lessonName['short'].length; i++) {
@@ -26,4 +57,6 @@ $(document).ready(function () {
 
 	$('.lesson__name').click(fullLessonName);
 	$('.now__name').click(fullLessonName);
+
+	$('.lesson__teacher').click(fullTeacherName);
 });
--- #source/js/main.js
+++ #source/js/main.js
@@ -1,3 +1,4 @@
+// @prepros-append "schedule.js"
 // @prepros-append "lessonTime.js"
 // @prepros-append "date.js"
 // @prepros-append "tabs.js"
